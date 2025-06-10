3c6327e4f242acf3e8133351d732978b697fecf4
abdelwadoud <abdubesh8@gmail.com>
2025-06-10 20:07:27 +0200

    Estructura del repositorio cambiada

--- demos/01-hola-mundo/HelloWorld.java
+++ demos/01-hola-mundo/HelloWorld.java
@@ -1,7 +1,10 @@
-public class HelloWorld {public static void main(String[] args) {
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
         String greetings = "Hola Mundo";
         System.out.println("-----------");
         System.out.println(greetings);
+        System.out.println("-----------");
     }
-    
 }
