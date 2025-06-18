abfcc8d3e44f4f3c0e15a009f65ea21aa073c9a8
abdelwadoud <abdubesh8@gmail.com>
2025-06-18 18:02:13 +0200

    numbers

---
+++ demos/ejercecios/numbers.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+import java.util.Random;
+public class numbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] opciones = {"piedra", "papel", "tijera"};
+
+        System.out.println("¡Bienvenido al juego Piedra, Papel o Tijera!");
+
+    }
+    
+}
